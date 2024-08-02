8e9d4d4a1d6691f4b9c354ef07c7b1c49f1d2002
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-08-02 11:03:06 -0300

    080224 aula0202

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 alert('Boas vindas ao jogos do número secreto!');
 let numeroSecreto = 5;
+console.log(numeroSecreto)
 let chute = prompt('Escolha um número entra 1 e 10');
 
+//se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-    console.log('Isso aí! Você descobriu o número secreto!');
+    alert('Isso aí! Você descobriu o número secreto!');
+} else {
+    alert('Você errou!')
 }
