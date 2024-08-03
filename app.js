f9a978e2405ada66f66518f5a1530846bc5d857a
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-08-03 11:10:05 -0300

    080324 aula0303

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 alert('Boas vindas ao jogos do número secreto!');
 let numeroSecreto = 4;
 console.log(numeroSecreto)
-let chute = prompt('Escolha um número entra 1 e 10');
+let chute = 
 
-//se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}!`);
-} else {
-    if (chute > numeroSecreto) {
-        alert(`O número secreto é menor que ${chute}`);
+//enquanto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entra 1 e 10');
+    //se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}!`);
     } else {
-        alert(`O número secreto é maior que ${chute}`);
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
     }
 }
