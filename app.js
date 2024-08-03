5183c9528234907b99f44a6da1a998d65369cb06
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-08-03 11:14:06 -0300

    080324 caula0304

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 alert('Boas vindas ao jogos do número secreto!');
 let numeroSecreto = 4;
-console.log(numeroSecreto)
-let chute = 
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
 //enquanto
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entra 1 e 10');
     //se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}!`);
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
         } else {
             alert(`O número secreto é maior que ${chute}`);
         }
+        //tentativas = tentativas + 1;
+        tentativas++;
     }
 }
